e6aeb22692401f33ef52e2804685004fbd17a2d0
Hallayus <hallayus@gmail.com>
2025-07-18 16:34:22 +0200

    added test to see if you are signed in to op

--- src/session.rs
+++ src/session.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 use anyhow::{Context, Result};
 
-use crate::wofi::message;
+use crate::wofi;
 
 pub type Session = String;
 
@@ -20,22 +20,38 @@ pub fn get_op_session_from_systemd() -> Result<Session> {
     }
 
     let env = String::from_utf8_lossy(&output.stdout);
-    for line in env.lines() {
-        if let Some((key, value)) = line.split_once('=') {
-            if key.starts_with("OP_SESSION") {
-                let session = value.to_string();
-                println!("{:?}", session);
-                return Ok(session);
+    let session = env
+        .lines()
+        .find_map(|line| {
+            let (key, value) = line.split_once('=')?;
+            if key == "OP_SESSION" {
+                Some(value.to_string())
+            } else {
+                None
             }
-        }
+        })
+        .context("No OP_SESSION environment variable found in systemd user environment")?;
+
+    let session_is_valid = is_session_valid(&session);
+
+    if session_is_valid {
+        Ok(session)
+    } else {
+        wofi::message(
+            "🔐 Not signed in to 1Password",
+            "Please run `op signin` in a terminal or the 1Password GUI.\nThis launcher requires an active OP_SESSION variable.",
+        )?;
+
+        Err(anyhow::anyhow!(
+            "No OP_SESSION variable found in systemd environment"
+        ))
     }
+}
 
-    message(
-        "🔐 Not signed in to 1Password",
-        "Please run `op signin` in a terminal or the 1Password GUI.\n\nThis launcher requires an active OP_SESSION variable.",
-    )?;
-
-    Err(anyhow::anyhow!(
-        "No OP_SESSION_* variable found in systemd environment"
-    ))
+fn is_session_valid(session: &Session) -> bool {
+    Command::new("op")
+        .args(["item", "list", "--session", &session, "--format", "json"])
+        .output()
+        .map(|output| output.status.success())
+        .unwrap_or(false)
 }
