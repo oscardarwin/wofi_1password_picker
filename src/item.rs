8e073af6b212f0784e27bea3422c4997548dd272
Hallayus <hallayus@gmail.com>
2025-07-18 22:35:26 +0200

    added support for otp

--- src/item.rs
+++ src/item.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, anyhow};
 use serde::Deserialize;
 
 use crate::{item_description::ItemDescription, session::Session, wofi};
@@ -40,6 +40,7 @@ pub struct Field {
     pub purpose: Option<String>,
     pub label: String,
     pub value: Option<String>,
+    pub totp: Option<String>,
     pub reference: Option<String>,
     pub password_details: Option<PasswordDetails>,
 }
@@ -49,7 +50,14 @@ pub struct PasswordDetails {
     pub strength: Option<String>,
 }
 
+#[derive(Debug, Deserialize)]
+struct DisplayedField {
+    pub label: String,
+    pub value: String,
+}
+
 fn get_item_from_1password(session: &Session, item_desc: &ItemDescription) -> Result<Item> {
+    println!("Grabbing item with id {}", item_desc.id);
     let output = Command::new("op")
         .args([
             "item",
@@ -70,33 +78,39 @@ pub fn select_field_to_copy(
     session: &Session,
     item_description: &ItemDescription,
 ) -> Result<String> {
-    let item = get_item_from_1password(session, item_description)?;
-    let fields = item.fields.as_ref().context("Item has no fields")?;
-    let mut displayable_fields = get_displayable_fields(&fields);
-    sort_fields_by_priority(&mut displayable_fields);
-    let labels_and_values = get_field_table(&displayable_fields)?;
+    let Item {
+        fields: Some(fields),
+        ..
+    } = get_item_from_1password(session, item_description)?
+    else {
+        return Err(anyhow!("Item has no fields"));
+    };
+    let mut displayed_fields = get_displayed_fields(fields);
+    sort_fields_by_priority(&mut displayed_fields);
+    let labels_and_values = get_field_table(&displayed_fields)?;
 
     let index = wofi::select("📋 Copy field", labels_and_values)?;
 
-    let selected_item = displayable_fields
+    let selected_item = displayed_fields
         .iter()
         .nth(index)
-        .and_then(|f| f.value.clone())
+        .map(|displayed_field| displayed_field.value.clone())
         .context("No value found for selected field")?;
 
     Ok(selected_item)
 }
 
-fn get_field_table(fields: &Vec<&Field>) -> Result<Vec<[String; 2]>> {
+fn get_field_table(fields: &Vec<DisplayedField>) -> Result<Vec<[String; 2]>> {
     let labels_and_values: Vec<[String; 2]> = fields
         .iter()
-        .filter_map(|label_and_value| {
-            let label = label_and_value.label.clone();
-            let display_value = match label_and_value.purpose.as_deref() {
-                Some("PASSWORD") => Some("********".to_string()),
-                _ => label_and_value.value.clone(),
-            }?;
-            Some([label, display_value])
+        .map(|displayed_field| {
+            let label = displayed_field.label.clone();
+            let display_value = match displayed_field.label.as_str() {
+                "one-time password" => "******".to_string(),
+                "password" => "********".to_string(),
+                _ => displayed_field.value.clone(),
+            };
+            [label, display_value]
         })
         .collect();
 
@@ -107,18 +121,33 @@ fn get_field_table(fields: &Vec<&Field>) -> Result<Vec<[String; 2]>> {
     Ok(labels_and_values)
 }
 
-fn get_displayable_fields(fields: &Vec<Field>) -> Vec<&Field> {
-    fields.iter().filter(|f| f.value.is_some()).collect()
+fn get_displayed_fields(fields: Vec<Field>) -> Vec<DisplayedField> {
+    fields
+        .into_iter()
+        .filter_map(|field| match field {
+            Field {
+                label,
+                totp: Some(totp),
+                ..
+            } => Some(DisplayedField { label, value: totp }),
+            Field {
+                label,
+                value: Some(value),
+                ..
+            } => Some(DisplayedField { label, value }),
+            _ => None,
+        })
+        .collect()
 }
 
-pub fn sort_fields_by_priority(fields: &mut Vec<&Field>) {
-    fn priority(purpose: Option<&str>) -> usize {
-        match purpose {
-            Some("PASSWORD") => 0,
-            Some("OTP") | Some("ONE_TIME_PASSWORD") => 1,
+pub fn sort_fields_by_priority(fields: &mut Vec<DisplayedField>) {
+    fn priority(label: &str) -> usize {
+        match label {
+            "password" => 0,
+            "one-time password" => 1,
             _ => 2,
         }
     }
 
-    fields.sort_by_key(|f| priority(f.purpose.as_deref()));
+    fields.sort_by_key(|f| priority(&f.label));
 }
